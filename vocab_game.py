01590a0f029c733d9cc0f3bb292df66d3766251b
409-19-kunyawee <54364@yupparaj.ac.th>
2026-08-25 14:31:53 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -19,7 +19,7 @@ def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
     st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
-    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 4
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
 
