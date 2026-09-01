c24a590a2cf496f75714203f827a507a43e2e706
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-01 14:00:38 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -35,7 +35,7 @@ def show_result_dialog(ans1, ans2, ans3,ans4):
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
     u_ans3 = ans3.strip().lower()
-    u_ans3 = ans4.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -54,14 +54,14 @@ def show_result_dialog(ans1, ans2, ans3,ans4):
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
     
     # ตรวจข้อ 3
-    if u_ans1 == "cherry":
+    if u_ans3 == "cherry":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
         
     # ตรวจข้อ 4
-    if u_ans2 == "mango":
+    if u_ans4 == "mango":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
