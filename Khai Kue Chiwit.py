c7c52d75880d055c8c61640a212e9a0d0a3c6a2d
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-15 14:17:49 +0700

    Update Khai Kue Chiwit.py

--- Khai Kue Chiwit.py
+++ Khai Kue Chiwit.py
@@ -3,3 +3,8 @@ st.markdown("# :orange[🍳 Khai Kue Chiwit 🍴]")
 
 st.divider()
 st.write("เมนูคนชอบข่าย")
+
+st.write("เครปไข่เจียว         ราคา 35 บาท")
+st.write("ซุปไข่ข้นมะเขือเทศ    ราคา 40 บาท")
+st.write("ไข่ตุ๋นหมูเด้ง         ราคา 40 บาท")
+st.write("ไข่ลูกเขยระเบิด       ราคา 39 บาท")
