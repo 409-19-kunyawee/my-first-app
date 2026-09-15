b318aad438c42fcc9a2b5bb4ed3160b7f8ed7981
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-15 14:25:11 +0700

    Update Khai Kue Chiwit.py

--- Khai Kue Chiwit.py
+++ Khai Kue Chiwit.py
@@ -8,3 +8,9 @@ with st.container(border=True):
     st.write("- ซุปไข่ข้นมะเขือเทศ    ราคา 40 บาท")
     st.write("- ไข่ตุ๋นหมูเด้ง         ราคา 40 บาท")
     st.write("- ไข่ลูกเขยระเบิด       ราคา 39 บาท")
+
+with st.container(border=True):
+    st.subheader("ส่วนลดของทางร้าน")
+    st.write("- ซื้อครบ 300 บาท ลด 15%")
+    st.write("- ซื้อทั้ง 4 เมนู ลดเพิ่มอีก 34 บาท")
+    
