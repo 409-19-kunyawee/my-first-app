52093c56a931990b6656e52c1e07ce08207b7967
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-20 21:25:25 +0700

    Update Khai Kue Chiwit.py

--- Khai Kue Chiwit.py
+++ Khai Kue Chiwit.py
@@ -74,6 +74,7 @@ min_amount_for_discount = 300.0
 if total_price < min_amount_for_discount:
     st.warning(f"ยอดซื้อยังไม่ถึง {min_amount_for_discount:,.0f} บาท ไม่สามารถใช้ส่วนลดได้")
     net_price = total_price
+    st.write(f"*ยอดชำระสุทธิ:* {net_price:,.2f} บาท")
 else:
     # เงื่อนไขเมื่อถึงยอดขั้นต่ำ (เช่น ลด 10%)
     discount = total_price * 0.10
