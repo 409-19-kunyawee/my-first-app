f90714c14072c66a68d64d1bcce4e15416a7fc73
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-21 20:36:05 +0700

    Update test two.py

--- test two.py
+++ test two.py
@@ -1,20 +1,44 @@
-import pandas as pd
+import streamlit as st
 
-# Load your data
-df = pd.read_csv('sales_data.csv')
+st.set_page_config(page_title="My Gallery")
 
-# Create a basic pivot table
-pivot = pd.pivot_table(
-    df,
-    values='Sales',
-    index='Region',
-    columns='Product',
-    aggfunc='sum',
-    fill_value=0
-)
+st.title("My Gallery")
 
-# Display the pivot table
-print(pivot)
+names = [
+    "NAME 1", "NAME 2", "NAME 3",
+    "NAME 4", "NAME 5", "NAME 6",
+    "NAME 7", "NAME 8", "NAME 9"
+]
 
-# Export to Excel
-pivot.to_excel('sales_pivot.xlsx', sheet_name='Sales Summary')
+for row in range(3):
+    col1, col2, col3 = st.columns(3)
+
+    for col, i in zip(
+        [col1, col2, col3],
+        range(row * 3, row * 3 + 3)
+    ):
+        with col:
+            st.markdown(
+                f"""
+                <div style="
+                    border: 1px solid black;
+                    padding: 30px;
+                    margin: 5px;
+                    text-align: center;
+                    height: 250px;
+                ">
+                    <div style="
+                        border: 1px solid black;
+                        height: 150px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    ">
+                        PHOTO
+                    </div>
+
+                    <h3>{names[i]}</h3>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
