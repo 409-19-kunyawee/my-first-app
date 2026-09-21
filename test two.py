0971b96b2b401f64784482df8c18650cf9e8c8c6
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-21 21:04:32 +0700

    Update test two.py

--- test two.py
+++ test two.py
@@ -31,7 +31,7 @@ names = [
 # 🔵 โซนที่ 2 : เปลี่ยนลิงก์รูป
 # ==============================
 images = [
-    "ใส่ลิงก์รูปที่ 1",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ29F-T1HQ2s2zmapwl_DbULhFF76I5b9fv-NNEVRsJcZ6ITnTeF5ka8Q8&s=10",
     "ใส่ลิงก์รูปที่ 2",
     "ใส่ลิงก์รูปที่ 3",
     "ใส่ลิงก์รูปที่ 4",
