e044e2a818a161bff04adfe04712c9ba8e943926
409-19-kunyawee <54364@yupparaj.ac.th>
2026-09-21 21:16:03 +0700

    Update for test.py

--- for test.py
+++ for test.py
@@ -130,6 +130,7 @@ if st.session_state.selected_items:
 
            if st.button("🧾 ออกใบเสร็จและคำนวณเงิน", type="primary", use_container_width=True):
         st.session_state.calc = True
+        
         # สรุปยอดเงิน
         st.write(f"*รวมเป็นเงิน (Subtotal):* {subtotal:,.2f} บาท")
         if discount_percent > 0:
